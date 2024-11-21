f102214b6ae706e70f4a07739cdfdbe7278a9f5a
Petr Broz <petr.broz@autodesk.com>
2024-11-21 12:48:48 +0100

    Trying the new express v5.

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,8 @@ const { getViewerToken } = require('../services/aps.js');
 
 let router = express.Router();
 
-router.get('/api/auth/token', async function (req, res, next) {
-    try {
-        res.json(await getViewerToken());
-    } catch (err) {
-        next(err);
-    }
+router.get('/api/auth/token', async function (req, res) {
+    res.json(await getViewerToken());
 });
 
 module.exports = router;
--- routes/models.js
+++ routes/models.js
@@ -4,58 +4,46 @@ const { listObjects, uploadObject, translateObject, getManifest, urnify } = requ
 
 let router = express.Router();
 
-router.get('/api/models', async function (req, res, next) {
-    try {
-        const objects = await listObjects();
-        res.json(objects.map(o => ({
-            name: o.objectKey,
-            urn: urnify(o.objectId)
-        })));
-    } catch (err) {
-        next(err);
-    }
+router.get('/api/models', async function (req, res) {
+    const objects = await listObjects();
+    res.json(objects.map(o => ({
+        name: o.objectKey,
+        urn: urnify(o.objectId)
+    })));
 });
 
-router.get('/api/models/:urn/status', async function (req, res, next) {
-    try {
-        const manifest = await getManifest(req.params.urn);
-        if (manifest) {
-            let messages = [];
-            if (manifest.derivatives) {
-                for (const derivative of manifest.derivatives) {
-                    messages = messages.concat(derivative.messages || []);
-                    if (derivative.children) {
-                        for (const child of derivative.children) {
-                            messages.concat(child.messages || []);
-                        }
+router.get('/api/models/:urn/status', async function (req, res) {
+    const manifest = await getManifest(req.params.urn);
+    if (manifest) {
+        let messages = [];
+        if (manifest.derivatives) {
+            for (const derivative of manifest.derivatives) {
+                messages = messages.concat(derivative.messages || []);
+                if (derivative.children) {
+                    for (const child of derivative.children) {
+                        messages.concat(child.messages || []);
                     }
                 }
             }
-            res.json({ status: manifest.status, progress: manifest.progress, messages });
-        } else {
-            res.json({ status: 'n/a' });
         }
-    } catch (err) {
-        next(err);
+        res.json({ status: manifest.status, progress: manifest.progress, messages });
+    } else {
+        res.json({ status: 'n/a' });
     }
 });
 
-router.post('/api/models', formidable({ maxFileSize: Infinity }), async function (req, res, next) {
+router.post('/api/models', formidable({ maxFileSize: Infinity }), async function (req, res) {
     const file = req.files['model-file'];
     if (!file) {
         res.status(400).send('The required field ("model-file") is missing.');
         return;
     }
-    try {
-        const obj = await uploadObject(file.name, file.path);
-        await translateObject(urnify(obj.objectId), req.fields['model-zip-entrypoint']);
-        res.json({
-            name: obj.objectKey,
-            urn: urnify(obj.objectId)
-        });
-    } catch (err) {
-        next(err);
-    }
+    const obj = await uploadObject(file.name, file.path);
+    await translateObject(urnify(obj.objectId), req.fields['model-zip-entrypoint']);
+    res.json({
+        name: obj.objectKey,
+        urn: urnify(obj.objectId)
+    });
 });
 
 module.exports = router;
