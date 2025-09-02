56c74fdfcb188e9730e6af922b64a7c46e5274b6
MarioPena48 <mario.anuar@gmail.com>
2025-09-01 22:41:51 -0600

    Autodesk_v2

--- routes/models.js
+++ routes/models.js
@@ -16,24 +16,6 @@ router.get('/models', async function (req, res, next) {
     }
 });
 
-const express = require('express');
-const formidable = require('express-formidable');
-const { listObjects, uploadObject, translateObject, getManifest, urnify } = require('../services/aps.js');
-
-let router = express.Router();
-
-router.get('/models', async function (req, res, next) {
-    try {
-        const objects = await listObjects();
-        res.json(objects.map(o => ({
-            name: o.objectKey,
-            urn: urnify(o.objectId)
-        })));
-    } catch (err) {
-        next(err);
-    }
-});
-
 router.get('/models/:urn/status', async function (req, res, next) {
     try {
         const manifest = await getManifest(req.params.urn);
@@ -59,27 +41,6 @@ router.get('/models/:urn/status', async function (req, res, next) {
 });
 
 router.post('/models', formidable({ maxFileSize: Infinity }), async function (req, res, next) {
-    const file = req.files['model-file'];
-    if (!file) {
-        res.status(400).send('The required field ("model-file") is missing.');
-        return;
-    }
-    try {
-        const obj = await uploadObject(file.name, file.path);
-        await translateObject(urnify(obj.objectId), req.fields['model-zip-entrypoint']);
-        res.json({
-            name: obj.objectKey,
-            urn: urnify(obj.objectId)
-        });
-    } catch (err) {
-        next(err);
-    }
-});
-
-module.exports = router;
-
-
-router.post('/api/models', formidable({ maxFileSize: Infinity }), async function (req, res, next) {
     const file = req.files['model-file'];
     if (!file) {
         res.status(400).send('The required field ("model-file") is missing.');
