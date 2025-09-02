5212e13c8aeb096b6e7e54010b434958603bdb1c
MarioPena48 <mario.anuar@gmail.com>
2025-09-01 23:21:28 -0600

    Autodesk_v2

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -10,7 +10,7 @@ async function setupModelSelection(viewer, selectedUrn) {
     const dropdown = document.getElementById('models');
     dropdown.innerHTML = '';
     try {
-        const resp = await fetch('/api/models');
+        const resp = await fetch('/api/aps/models');
         if (!resp.ok) {
             throw new Error(await resp.text());
         }
@@ -43,7 +43,7 @@ async function setupModelUpload(viewer) {
         models.setAttribute('disabled', 'true');
         showNotification(`Uploading model <em>${file.name}</em>. Do not reload the page.`);
         try {
-            const resp = await fetch('/api/models', { method: 'POST', body: data });
+            const resp = await fetch('/api/aps/models', { method: 'POST', body: data });
             if (!resp.ok) {
                 throw new Error(await resp.text());
             }
@@ -68,7 +68,7 @@ async function onModelSelected(viewer, urn) {
     }
     window.location.hash = urn;
     try {
-        const resp = await fetch(`/api/models/${urn}/status`);
+        const resp = await fetch(`/api/aps/models/${urn}/status`);
         if (!resp.ok) {
             throw new Error(await resp.text());
         }
